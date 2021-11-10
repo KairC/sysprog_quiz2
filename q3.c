7414a9134774ea687de5f9765e3b728ae84350ae
KairC <kai0902@hotmail.com>
2021-11-11 00:28:24 +0800

    Finish q4

--- q3.c
+++ q3.c
@@ -3,24 +3,16 @@
 const uint32_t D = 3;
 #define M ((uint64_t)(UINT64_C(0xFFFFFFFFFFFFFFFF) / (D) + 1))
 
-uint32_t quickmod(uint32_t n) {
-  uint64_t quotient = ((__uint128_t)M * n) >> 64;
-  return n - quotient * D;
+uint32_t quickmod(uint32_t n)
+{
+    uint64_t quotient = ((__uint128_t) M * n) >> 64;
+    return n - quotient * D;
 }
 
-int main() {
-  uint32_t tt = 4294967295;
-  printf("%u\n", quickmod(55));
-  uint64_t test = 0xFFFFFFFFFFFFFFFF;
-  printf("%lx\n%lx\n%lx %lx\n", test, test / D,
-         (uint64_t)(((__uint128_t)tt * (test / D + 1)) >> 64),
-         tt * (test / D + 1));
-  printf("================\n%lx\n%lx\n%lx %lx\n", test, test / D,
-         (uint64_t)(((__uint128_t)tt * (test / D)) >> 64), tt * (test / D));
-
-  printf("================\n%lx %lx\n%lx\n",
-         (uint64_t)(((__uint128_t)((__uint128_t)test + D) / D) >> 64),
-         (uint64_t)((__uint128_t)((__uint128_t)test + D) / D), M);
-
-  return 0;
+int main()
+{
+    printf("D = 3 , quickmod(5) will get : %u\n", quickmod(5));
+    printf("D = 3 , quickmod(55) will get : %u\n", quickmod(55));
+    printf("D = 3 , quickmod(555) will get : %u\n", quickmod(555));
+    return 0;
 }
--- q4.c
+++ q4.c
@@ -1,3 +1,23 @@
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+const uint32_t D = 3;
+#define M ((uint64_t)(UINT64_C(0xFFFFFFFFFFFFFFFF) / (D) + 1))
 
-int main() { return 0; }
+bool divisible(uint32_t n)
+{
+    return n * M <= M - 1;
+}
+
+uint32_t quickmod(uint32_t n)
+{
+    uint64_t quotient = ((__uint128_t) M * n) >> 64;
+    return n - quotient * D;
+}
+
+int main()
+{
+    printf("D=3 , divisible(7) will get : %d\n", divisible(7));
+    printf("D=3 , divisible(87) will get : %d\n", divisible(87));
+    return 0;
+}
